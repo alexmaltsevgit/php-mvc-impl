e3a0998b6d7d874b916cde033ed0db7e3b2a1b87
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-10-15 22:34:32 +0300

    implemented BaseController

--- core/MVC/BaseController.php
+++ core/MVC/BaseController.php
@@ -5,5 +5,12 @@ namespace Core\MVC;
 
 
 class BaseController {
+  private function render($view_name, $data = null, $template = '') {
+    $view = new BaseView();
+    if ($template) {
+      $view->set_template($template);
+    }
 
+    $view->render($view_name, $data);
+  }
 }
